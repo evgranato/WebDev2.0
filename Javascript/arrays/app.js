739909d755b5de73a3c183901fe1282b3d3ba7c8
Evan Granato <evgranato@gmail.com>
2021-03-03 14:22:44 -0600

    JS Arrays and Objects

---
+++ Javascript/arrays/app.js
@@ -0,0 +1,18 @@
+console.log("Working!");
+
+let songList = [];
+// let i = 0;
+
+// while (i <= 10) {
+//   let input = prompt("Next Song");
+//   songList.push(input);
+//   i++;
+// }
+// console.log(songList);
+
+for (let i = 0; i < 5; i++) {
+  let input = prompt("Next Song");
+  songList.push(input);
+}
+console.log(songList);
+console.log(songList[0]);

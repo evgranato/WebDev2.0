5050c387db0d53ece48d56d3610a2e34dd2bf57f
Evan Granato <evgranato@gmail.com>
2021-03-05 20:48:16 -0600

    while loops

---
+++ Javascript/loops/app.js
@@ -0,0 +1,336 @@
+console.log("Working");
+
+// const animals = ["lions", "tigers", "bears"];
+
+// for (let i = 0; i < animals.length; i++) {
+//   console.log(i, animals[i]);
+// }
+
+// const longAnimals = [
+//   "Aardvark",
+//   "Albatross",
+//   "Alligator",
+//   "Alpaca",
+//   "Ant",
+//   "Anteater",
+//   "Antelope",
+//   "Ape",
+//   "Armadillo",
+//   "Donkey",
+//   "Baboon",
+//   "Badger",
+//   "Barracuda",
+//   "Bat",
+//   "Bear",
+//   "Beaver",
+//   "Bee",
+//   "Bison",
+//   "Boar",
+//   "Buffalo",
+//   "Butterfly",
+//   "Camel",
+//   "Capybara",
+//   "Caribou",
+//   "Cassowary",
+//   "Cat",
+//   "Caterpillar",
+//   "Cattle",
+//   "Chamois",
+//   "Cheetah",
+//   "Chicken",
+//   "Chimpanzee",
+//   "Chinchilla",
+//   "Chough",
+//   "Clam",
+//   "Cobra",
+//   "Cockroach",
+//   "Cod",
+//   "Cormorant",
+//   "Coyote",
+//   "Crab",
+//   "Crane",
+//   "Crocodile",
+//   "Crow",
+//   "Curlew",
+//   "Deer",
+//   "Dinosaur",
+//   "Dog",
+//   "Dogfish",
+//   "Dolphin",
+//   "Dotterel",
+//   "Dove",
+//   "Dragonfly",
+//   "Duck",
+//   "Dugong",
+//   "Dunlin",
+//   "Eagle",
+//   "Echidna",
+//   "Eel",
+//   "Eland",
+//   "Elephant",
+//   "Elk",
+//   "Emu",
+//   "Falcon",
+//   "Ferret",
+//   "Finch",
+//   "Fish",
+//   "Flamingo",
+//   "Fly",
+//   "Fox",
+//   "Frog",
+//   "Gaur",
+//   "Gazelle",
+//   "Gerbil",
+//   "Giraffe",
+//   "Gnat",
+//   "Gnu",
+//   "Goat",
+//   "Goldfinch",
+//   "Goldfish",
+//   "Goose",
+//   "Gorilla",
+//   "Goshawk",
+//   "Grasshopper",
+//   "Grouse",
+//   "Guanaco",
+//   "Gull",
+//   "Hamster",
+//   "Hare",
+//   "Hawk",
+//   "Hedgehog",
+//   "Heron",
+//   "Herring",
+//   "Hippopotamus",
+//   "Hornet",
+//   "Horse",
+//   "Human",
+//   "Hummingbird",
+//   "Hyena",
+//   "Ibex",
+//   "Ibis",
+//   "Jackal",
+//   "Jaguar",
+//   "Jay",
+//   "Jellyfish",
+//   "Kangaroo",
+//   "Kingfisher",
+//   "Koala",
+//   "Kookabura",
+//   "Kouprey",
+//   "Kudu",
+//   "Lapwing",
+//   "Lark",
+//   "Lemur",
+//   "Leopard",
+//   "Lion",
+//   "Llama",
+//   "Lobster",
+//   "Locust",
+//   "Loris",
+//   "Louse",
+//   "Lyrebird",
+//   "Magpie",
+//   "Mallard",
+//   "Manatee",
+//   "Mandrill",
+//   "Mantis",
+//   "Marten",
+//   "Meerkat",
+//   "Mink",
+//   "Mole",
+//   "Mongoose",
+//   "Monkey",
+//   "Moose",
+//   "Mosquito",
+//   "Mouse",
+//   "Mule",
+//   "Narwhal",
+//   "Newt",
+//   "Nightingale",
+//   "Octopus",
+//   "Okapi",
+//   "Opossum",
+//   "Oryx",
+//   "Ostrich",
+//   "Otter",
+//   "Owl",
+//   "Oyster",
+//   "Panther",
+//   "Parrot",
+//   "Partridge",
+//   "Peafowl",
+//   "Pelican",
+//   "Penguin",
+//   "Pheasant",
+//   "Pig",
+//   "Pigeon",
+//   "Pony",
+//   "Porcupine",
+//   "Porpoise",
+//   "Quail",
+//   "Quelea",
+//   "Quetzal",
+//   "Rabbit",
+//   "Raccoon",
+//   "Rail",
+//   "Ram",
+//   "Rat",
+//   "Raven",
+//   "Red deer",
+//   "Red panda",
+//   "Reindeer",
+//   "Rhinoceros",
+//   "Rook",
+//   "Salamander",
+//   "Salmon",
+//   "Sand Dollar",
+//   "Sandpiper",
+//   "Sardine",
+//   "Scorpion",
+//   "Seahorse",
+//   "Seal",
+//   "Shark",
+//   "Sheep",
+//   "Shrew",
+//   "Skunk",
+//   "Snail",
+//   "Snake",
+//   "Sparrow",
+//   "Spider",
+//   "Spoonbill",
+//   "Squid",
+//   "Squirrel",
+//   "Starling",
+//   "Stingray",
+//   "Stinkbug",
+//   "Stork",
+//   "Swallow",
+//   "Swan",
+//   "Tapir",
+//   "Tarsier",
+//   "Termite",
+//   "Tiger",
+//   "Toad",
+//   "Trout",
+//   "Turkey",
+//   "Turtle",
+//   "Viper",
+//   "Vulture",
+//   "Wallaby",
+//   "Walrus",
+//   "Wasp",
+//   "Weasel",
+//   "Whale",
+//   "Wildcat",
+//   "Wolf",
+//   "Wolverine",
+//   "Wombat",
+//   "Woodcock",
+//   "Woodpecker",
+//   "Worm",
+//   "Wren",
+//   "Yak",
+//   "Zebra",
+// ];
+
+// // for (let i = 0; i < longAnimals.length; i++) {
+// //   console.log(i, longAnimals[i]);
+// // }
+
+// for (let i = longAnimals.length - 1; i >= 0; i--) {
+//   console.log(i, longAnimals[i]);
+// }
+
+// const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// // WRITE YOUR LOOP BELOW THIS LINE:
+// for (let i = 0; i <= people.length; i++) {
+//   console.log(people[i].toUpperCase);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`i is: ${i}`);
+//   for (let j = 1; j < 4; j++) {
+//     console.log(`      j is: ${j}`);
+//   }
+// }
+
+// const seatingChart = [
+//   ["Kristen", "Erik", "Namita"],
+//   ["Geoffrey", "Juanita", "Antonio", "Kevin"],
+//   ["Yuma", "Sakura", "Jack", "Erika"],
+// ];
+
+// for (let outer = 0; outer < seatingChart.length; outer++) {
+//   console.log(`Row ${outer + 1}`);
+//   for (let inner = 0; inner < seatingChart[outer].length; inner++) {
+//     console.log(seatingChart[outer][inner]);
+//   }
+// }
+
+// WHILE LOOPS
+
+// let count = 0;
+// while (count < 10) {
+//   count++;
+//   console.log(count);
+// }
+
+// const secret = "BabyHippo";
+
+// let guess = prompt("Enter the secret code");
+
+// while (guess != secret) {
+//   guess = prompt("Enter the secret code");
+// }
+// console.log("You guessed it.");
+
+// let input = prompt("Hey, say something!");
+
+// while (true) {
+//   input = prompt(input);
+//   if (input.toLowerCase() === "stop copying me") {
+//     break;
+//   }
+// }
+// console.log("OK, YOU WIN!");
+
+let maximum = parseInt(prompt("Enter the maximum number!"));
+while (!maximum) {
+  maximum = parseInt(prompt("Enter a valid number!"));
+}
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+console.log(targetNum);
+
+let guess = parseInt(prompt("Enter your first guess"));
+
+let guessCount = 1;
+
+while (parseInt(guess) !== targetNum) {
+  if (guess === "q") break;
+  guessCount++;
+  if (guess > targetNum) {
+    guess = prompt("Too High. Guess again");
+  } else {
+    guess = prompt("Too Low. Guess again");
+  }
+}
+if (guess === "q") {
+  alert("OK, you quit");
+} else {
+  alert(`You guessed the number, it took you ${guessCount} guesses`);
+}
+
+// let guess = prompt("Guess the number");
+
+// while (guess != 10) {
+//   if (guess <= 0 && guess <= 9) {
+//     console.log("Higher");
+//     guess = prompt("Guess the number");
+//   } else if (guess >= 11 && guess >= 20) {
+//     console.log("Lower");
+//     guess = prompt("Guess the number");
+//   }
+// }
+// console.log("You guessed the number");

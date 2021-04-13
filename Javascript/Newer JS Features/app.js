822f318c8434ac7112459f8e7b5bbe772d27ef6a
Evan Granato <evgranato@gmail.com>
2021-04-13 14:08:55 -0500

    Newer JS Features completed

--- Javascript/Newer JS Features/app.js
+++ Javascript/Newer JS Features/app.js
@@ -10,12 +10,12 @@ function rollDie(numSides = 6) {
   return Math.floor(Math.random() * numSides) + 1;
 }
 
-console.log(Math.max(1, 2, 4, 11, 405, 21, 1));
+//console.log(Math.max(1, 2, 4, 11, 405, 21, 1));
 //PRINTS 405
 const nums = [1, 2, 4, 11, 405, 21, 1];
-console.log(Math.max(nums));
+//console.log(Math.max(nums));
 //PRINTS NaN
-console.log(Math.max(...nums));
+//console.log(Math.max(...nums));
 //PRINTS 405
 
 const cats = ["Snickers", "Maggie"];
@@ -62,3 +62,89 @@ function raceResults(gold, silver, ...everyoneElse) {
   console.log(`SILVER MEDAL GOES TO: ${silver}`);
   console.log(`AND THANKS TO EVERYONE ELSE: ${everyoneElse}`);
 }
+
+const scores = [929321, 899341, 888336, 772739, 543671, 243567, 111934];
+
+const [gold, silver, bronze] = scores;
+// gold = 929321
+//silver = 899341
+//bronze = 888336
+
+const user = {
+  email: "runner@gmail.com",
+  password: "runningislife",
+  firstName: "Eliud",
+  lastName: "Kipchoge",
+  born: 1980,
+  died: undefined,
+  bio: "He runs really far and really fast.",
+  city: "New York",
+  state: "New York",
+};
+
+// const firstName = user.firstName;
+// const lastName = user.lastName;
+
+// const { email, firstName, lastName, bio, city } = user;
+
+// const { born: birthYear } = user;
+
+const user2 = {
+  email: "test@gmail.com",
+  firstName: "Stacy",
+  lastName: "Gonzalez",
+  born: 1987,
+  city: "Tulsa",
+  state: "Oklahoma",
+};
+
+const { city, state, died } = user2;
+
+// GOOD:
+function fullName() {
+  return `${user2.firstName} ${user2.lastName}`;
+}
+//BETTER:
+// function fullName() {
+//   const { firstName, lastName } = user2;
+//   return `${firstName} ${lastName}`;
+// }
+// //BEST:
+// function fullName({ firstName, lastName }) {
+//   return `${firstName} ${lastName}`;
+// }
+
+const movies = [
+  {
+    title: "Amadeus",
+    score: 99,
+    year: 1950,
+  },
+  {
+    title: "Stand By Me",
+    score: 60,
+    year: 1997,
+  },
+  {
+    title: "Parasite",
+    score: 95,
+    year: 2014,
+  },
+  {
+    title: "Alien",
+    score: 90,
+    year: 1979,
+  },
+];
+
+const bestMovies = movies.filter((movie) => movie.score >= 90);
+//BETTER:
+//const bestMovies = movies.filter(({ score }) => score >= 90);
+
+const ratings = movies.map((movie) => {
+  return `${movie.title} (${movie.year}) is rated ${movie.score}`;
+});
+//DESTRUCTURED:
+const destRatings = movies.map(
+  ({ title, score, year }) => `${title} (${year}) is rated ${score}`
+);

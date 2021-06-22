c44998a4c99c86ec29867efd80adfd3409bedbd7
Evan Granato <evgranato@gmail.com>
2021-06-22 09:17:23 -0500

    finished js classes

--- Prototypes, Classes & OOP/app.js
+++ Prototypes, Classes & OOP/app.js
@@ -51,32 +51,121 @@
 // const myColor = new Color(255, 40, 100);
 
 //CLASS SYNTAX
-class Color {
-  constructor(r, g, b, name) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
-    this.name = name;
+// class Color {
+//   constructor(r, g, b, name) {
+//     this.r = r;
+//     this.g = g;
+//     this.b = b;
+//     this.name = name;
+//     this.calcHSL();
+//   }
+//   greet() {
+//     return `HELLO FROM ${this.name.toUpperCase()}`;
+//   }
+//   innerRGB() {
+//     const { r, g, b } = this;
+//     return `${r}, ${g}, ${b}`;
+//   }
+//   rgb() {
+//     const { r, g, b } = this;
+//     return `rgb(${this.innerRGB()})`;
+//   }
+//   hex() {
+//     const { r, g, b } = this;
+//     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+//   }
+//   rgba(a = 1) {
+//     return `rgba(${this.innerRGB()}, ${a})`;
+//   }
+//   hsl() {
+//     const { h, s, l } = this;
+//     return `hsl(${h}, ${s}%, ${l}%)`;
+//   }
+//   fullSaturation() {
+//     const { h, s, l } = this;
+//     return `hsl(${h}, ${100}%, ${l}%)`;
+//   }
+//   opposite() {
+//     const { h, s, l } = this;
+//     const newHue = (h + 180) % 360;
+//     return `hsl(${newHue}, ${s}%, ${l}%)`;
+//   }
+//   calcHSL() {
+//     let { r, g, b } = this;
+//     // Make r, g and b fractions of 1
+//     r /= 255;
+//     g /= 255;
+//     b /= 255;
+
+//     // Find greatest and smallest channel values
+//     let cmin = Math.min(r, g, b),
+//       cmax = Math.max(r, g, b),
+//       delta = cmax - cmin,
+//       h = 0,
+//       s = 0,
+//       l = 0;
+//     if (delta == 0) h = 0;
+//     else if (cmax == r)
+//       // Red is max
+//       h = ((g - b) / delta) % 6;
+//     else if (cmax == g)
+//       // Green is max
+//       h = (b - r) / delta + 2;
+//     // Blue is max
+//     else h = (r - g) / delta + 4;
+
+//     h = Math.round(h * 60);
+
+//     // Make negative hues positive behind 360º
+//     if (h < 0) h += 360;
+
+//     // Calculate lightness
+//     l = (cmax + cmin) / 2;
+
+//     // Calculate saturation
+//     s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+//     //Multiply l and s by 100
+//     s = +(s * 100).toFixed(1);
+//     l = +(l * 100).toFixed(1);
+//     this.h = h;
+//     this.s = s;
+//     this.l = l;
+//   }
+// }
+
+// const red = new Color(255, 67, 89, "tomato");
+// const white = new Color(255, 255, 255, "white");
+
+// HSL CLASS
+
+// EXTENDS AND SUPER KEYWORDS:
+
+class Pet {
+  constructor(name, age) {
+    this.name = name[0].toUpperCase() + name.substring(1);
+    this.age = age;
   }
-  greet() {
-    return `HELLO FROM ${this.name.toUpperCase()}`;
-  }
-  innerRGB() {
-    const { r, g, b } = this;
-    return `${r}, ${g}, ${b}`;
-  }
-  rgb() {
-    const { r, g, b } = this;
-    return `rgb(${this.innerRGB()})`;
-  }
-  hex() {
-    const { r, g, b } = this;
-    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-  }
-  rgba(a = 1) {
-    return `rgba(${this.innerRGB()}, ${a}`;
+  eat() {
+    return `${this.name} is eating.`;
   }
 }
 
-const red = new Color(255, 67, 89, "tomato");
-const white = new Color(255, 255, 255, "white");
+class Cat extends Pet {
+  constructor(name, age, livesLeft = 9) {
+    super(name, age);
+    this.livesLeft = livesLeft;
+  }
+  meow() {
+    return "MEOWWW!";
+  }
+  lifeLost() {
+    this.livesLeft -= 1;
+  }
+}
+
+class Dog extends Pet {
+  bark() {
+    return "WOOOF!";
+  }
+}

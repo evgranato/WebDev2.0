0883261adcff6c04d270177554fb220c30b05887
Evan Granato <evgranato@gmail.com>
2021-08-04 13:53:42 -0500

    working on RESTful Routes

--- RESTfulRoutes/index.js
+++ RESTfulRoutes/index.js
@@ -8,15 +8,56 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const comments = [
+  {
+    username: "Todd",
+    comment: "lol that is so funny!",
+  },
+  {
+    username: "Skyler",
+    comment: "I like to go birdwatching with my dog",
+  },
+  {
+    username: "Sk8erBoi",
+    comment: "Plz delete your account, Todd",
+  },
+  {
+    username: "onlysayswoof",
+    comment: "woof woof woof",
+  },
+];
+
+app.get("/comments", (req, res) => {
+  res.render("comments/index", { comments });
+});
+
+app.get("/comments/new", (req, res) => {
+  res.render("comments/new");
+});
+
+app.post("/comments", (req, res) => {
+  const { username, comment } = req.body;
+  comments.push({ username, comment });
+  res.render("comments/index", { comments });
+});
+
 app.get("/tacos", (req, res) => {
   res.send("GET /tacos RESPONSE");
 });
 
 app.post("/tacos", (req, res) => {
   const { meat, qty } = req.body;
-  res.send(`OK, HERE ARE YOUR ${qty} ${meat} tacos`);
+  res.send(
+    `OK, HERE ARE YOUR ${qty} ${meat[0].toUpperCase() + meat.slice(1)} Tacos`
+  );
 });
 
 app.listen(3000, () => {
   console.log("LISTENING ON PORT 3000");
 });
+
+// GET /comments - list all comments
+// POST /comments - Create a new comment
+// GET /comments/:id - Get one comment (using ID)
+// PATCH /comments/:id - Update one comment
+// DELETE /comments/:id - Destroy one comment

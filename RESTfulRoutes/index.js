2b14effa5139979652fc2517271cdde54ea7390a
Evan Granato <evgranato@gmail.com>
2021-08-05 13:13:44 -0500

    finished RESTful Routes

--- RESTfulRoutes/index.js
+++ RESTfulRoutes/index.js
@@ -1,31 +1,42 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4: uuid } = require("uuid");
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname + "/views"));
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method"));
 
-const comments = [
+let comments = [
   {
+    id: uuid(),
     username: "Todd",
     comment: "lol that is so funny!",
   },
   {
+    id: uuid(),
     username: "Skyler",
     comment: "I like to go birdwatching with my dog",
   },
   {
+    id: uuid(),
     username: "Sk8erBoi",
     comment: "Plz delete your account, Todd",
   },
   {
+    id: uuid(),
     username: "onlysayswoof",
     comment: "woof woof woof",
   },
 ];
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 app.get("/comments", (req, res) => {
   res.render("comments/index", { comments });
@@ -35,10 +46,43 @@ app.get("/comments/new", (req, res) => {
   res.render("comments/new");
 });
 
+// app.post("/comments", (req, res) => {
+//   const { username, comment } = req.body;
+//   const commId = comments[comments.length - 1].id;
+//   comments.push({ username, comment, id: commId + 1 });
+//   res.redirect("/comments");
+// });
+
 app.post("/comments", (req, res) => {
   const { username, comment } = req.body;
-  comments.push({ username, comment });
-  res.render("comments/index", { comments });
+  comments.push({ username, comment, id: uuid() });
+  res.redirect("/comments");
+});
+
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/show", { comment });
+});
+
+app.get("/comments/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/edit", { comment });
+});
+
+app.patch("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const newCommentText = req.body.comment;
+  const foundComment = comments.find((c) => c.id === id);
+  foundComment.comment = newCommentText;
+  res.redirect("/comments");
+});
+
+app.delete("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  comments = comments.filter((c) => c.id !== id);
+  res.redirect("/comments");
 });
 
 app.get("/tacos", (req, res) => {
@@ -55,9 +99,3 @@ app.post("/tacos", (req, res) => {
 app.listen(3000, () => {
   console.log("LISTENING ON PORT 3000");
 });
-
-// GET /comments - list all comments
-// POST /comments - Create a new comment
-// GET /comments/:id - Get one comment (using ID)
-// PATCH /comments/:id - Update one comment
-// DELETE /comments/:id - Destroy one comment

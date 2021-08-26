a396dc6177ccc32fce3e489508e6bc93c1bf04f1
Evan Granato <evgranato@gmail.com>
2021-08-26 12:16:19 -0500

    didnt save

---
+++ Mongo/staging.js
@@ -0,0 +1,52 @@
+[
+    {
+    name: "Charlie",
+    age: 2,
+    size: "M",
+    breed: "Lab", 
+    personality: {
+        catFriendly: true,
+        temperment: "sweet"
+    }
+},
+{
+    name: "Jack",
+    age: 2,
+    size: "L",
+    breed: "Golden Doodle", 
+    personality: {
+        catFriendly: true,
+        temperment: "sweet"
+    }
+},
+{
+    name: "Bentley",
+    age: 12,
+    size: "M",
+    breed: "Aussie", 
+    personality: {
+        catFriendly: true,
+        temperment: "aggressive"
+    }
+},
+{
+    name: "Lilly",
+    age: 11,
+    size: "S",
+    breed: "Mutt", 
+    personality: {
+        catFriendly: true,
+        temperment: "Terror"
+    }
+},
+{
+    name: "Patrick",
+    age: 4,
+    size: "S",
+    breed: "Havipoo", 
+    personality: {
+        catFriendly: true,
+        temperment: "Mixed"
+    }
+}
+]
